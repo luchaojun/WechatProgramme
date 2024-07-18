fd7e7dd7e4c6d5ed384f2e6cbf2010d3fd65901b
chaojun_Lu <chaojun_Lu@clevo.com.tw>
2024-07-18 16:55:41 +0800

    Add page list function

---
+++ pages/selectinfo3/selectinfo3.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    inputValue:'' ,
+    allItems:''
+  },
+  bindkeyInput: function(e){
+    this.setData({inputValue: e.detail.value});
+  },
+  select_btn: function(){
+    var lds = this;
+    wx.request({
+      url: 'http://172.16.7.61:8080/getProductProgressBySaleOrder',
+      data: {
+        saleOrder: this.data.inputValue
+      },
+      method: "GET",
+      success(res){
+        lds.setData({allItems: res.data});
+        console.log(lds.data.dataItems);
+      }
+    })
+  },
+  select_all: function(){
+    var lds = this;
+    wx.request({
+      url: 'http://172.16.7.61:8080/getAllProductProgress',
+      method: "GET",
+      success(res){
+        lds.setData({allItems: res.data});
+        console.log(lds.data.allItems);
+      }
+    })
+  }
+})
